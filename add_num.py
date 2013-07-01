5e876ce372cc41439493108ea77fcaa783465b9b
Irenea Lodangco <ireneacl@ou.edu>
2013-07-01 15:32:52 -0400

    Added some super important stuff

--- add_num.py
+++ add_num.py
@@ -1,9 +1,11 @@
+
 #This has been authored by Irenea but who knows who will contribute to it
 from adding import add_two, add_three
 
 def main():
     print ("2+2+2 =", add_three(2,2,2))
     print ("2 + 3 = ",add_two(2,3))
+    print "Hello, Oklahoma"
 
 if _name_== "_main_":
     main():
