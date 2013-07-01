380d8c4d58745571129ca9ae5806d08243a7e7de
Irenea Lodangco <ireneacl@ou.edu>
2013-07-01 15:29:50 -0400

    Addedd module with functions

--- add_num.py
+++ add_num.py
@@ -1,7 +1,8 @@
-def add_two(a,b):
-    return a+b
+#This has been authored by Irenea but who knows who will contribute to it
+from adding import add_two, add_three
 
 def main():
+    print ("2+2+2 =", add_three(2,2,2))
     print ("2 + 3 = ",add_two(2,3))
 
 if _name_== "_main_":
